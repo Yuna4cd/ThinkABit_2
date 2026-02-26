45c073cd9113559366af5c7dbd3a353db7bca0b8
kyungwan2023F <kyungwan@vt.edu>
2026-02-26 13:17:11 -0500

    added basic styling for navbar and homepage layout

--- thinkABit_2/src/components/Navbar.jsx
+++ thinkABit_2/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
+import './Navbar.css'
 
 export default function Navbar() {
     return (
--- thinkABit_2/src/pages/HomePage.jsx
+++ thinkABit_2/src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
-
+import './HomePage.css'
 
 export default function HomePage() {
-
     return (
-        <>
-        HomePage.
-        </>
+        <div className="home-container">
+            <div className="sidebar">
+                <div className="sidebar-item">HowToVisualization</div>
+                <div className="sidebar-item">Step 1</div>
+                <div className="sidebar-item">Step 2</div>
+                <div className="sidebar-item">Step 3</div>
+            </div>
+            <div className="main-content">
+                <p>dummy text</p>
+            </div>
+        </div>
     )
 }
