31a4685b30088a1bfd1998c7ebfe8dd34991ae08
dmgm818 <dmgm@vt.com>
2026-03-10 15:37:19 -0400

    reorganized backend folders and files

--- thinkABit_2/src/components/Navbar.jsx
+++ thinkABit_2/src/components/Navbar.jsx
@@ -1,15 +1,25 @@
 import { NavLink, Link } from 'react-router-dom'
 import './Navbar.css'
+import logo from '../assets/thinkabit_logo.png'
 
 export default function Navbar() {
+
+    const user = {
+        name: 'username',
+        avator: '../assets/react.svg', 
+    }
+
     return (
         <nav>
-            <Link to="/" className="logo-link">ThinkABit</Link>
-            <div>
-                <NavLink to="/upload">Visualization</NavLink>
-                <NavLink to="/documents">Documents</NavLink>
+            <Link to="/" className="logo-link"><img src={logo} alt='logo' className='logo-img'/></Link>
+            <div className='nav-btn'>
+                <NavLink to="/upload">Visualization<i class="fa-solid fa-chart-area"></i></NavLink>
+                <NavLink to="/documents">Documents<i class="fa-solid fa-file"></i></NavLink>
             </div>
-            <div>Username</div>
+            <div className='user-section'>
+                <span>{user.name}</span>
+                <img src={user.avator} alt='avator' className='avator'/>
+            </div>
         </nav>
     )
 }
